826cb77c6612f60abec0b9ee646c890db229e57c
Dawid <dawidmajka@protonmail.com>
2025-06-06 12:12:58 +0200

    Display single book

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+import books from "./src/dummyBooks";
+
+const book = books[0];
 
 export default function App() {
   return (
-    <View className="bg-slate-950 flex-1 items-center justify-center">
-      <Text className="text-2xl text-red-100 font-bold">We are live</Text>
+    <View className="bg-slate-950 flex-1 justify-center p-4">
+      {/* Book Row */}
+      <View className="flex-row gap-2 items-center">
+        <Image
+          source={{ uri: book.thumbnail_url }}
+          className="w-16 aspect-square rounded-md"
+        />
+        <View className="flex-col flex-1 gap-1">
+          <Text className="text-2xl text-gray-100 font-bold">{book.title}</Text>
+          <Text className="text-gray-400">{book.author}</Text>
+        </View>
+
+        <AntDesign
+          name="playcircleo"
+          size={24}
+          color="gainsboro"
+          className=""
+        />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
